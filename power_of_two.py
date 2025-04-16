49c3df8c31c39cf6b095ede4b7d57b6bd611e052
Jayden Smith <jayden.smith2@stu.ocsb.ca>
2025-04-16 12:54:17 +0000

    Added power_of_two.py

---
+++ power_of_two.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# Created By: Jayden Smith
+# Date: March 19, 2025
+# This program tells you the squares of the numbers up to the users number
+
+
+def main():
+
+    # Get the user number
+    user_number_as_string = input("Enter a positive whole number: ")
+    try:
+
+        # Converts user num to int
+        user_number_as_int = int(user_number_as_string)
+
+        # If its negative this happens
+        if user_number_as_int < 0:
+            print((user_number_as_int), "is not a positive integer")
+
+        # If not, code continues
+        else:
+
+            # calculate the square of each number from 0 to the user number
+            for counter in range(user_number_as_int + 1):
+                power_of_two = counter**2
+                print("{}^2 = {}".format(counter, power_of_two))
+
+    # If user num is not a int this happens
+    except Exception:
+        print((user_number_as_string), "is not a positive integer")
+
+
+if __name__ == "__main__":
+    main()
